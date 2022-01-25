bbc1a584a96f5013fbf971a8b592e9e415ca44ed
SudhanshuSingh3 <ss.sudhanshu333@gmail.com>
2022-01-25 13:48:50 +0530

    Create Recursion.js

---
+++ Recursion.js
@@ -0,0 +1,88 @@
+//Recursion is a process calling itself. A Function that calls itself is called a recursive function
+
+// 1 to 10
+// function fun(num){
+//     if(num>0){
+//        // num=num-1;
+//         fun(num-1);
+//         console.log(num);
+//     }
+//     return;
+    
+
+// }
+
+// fun(10);
+
+
+//print 4 to 30
+
+// function fun(num){
+// if(num>=4){
+//     fun(num-1)
+//     console.log(num);
+// }
+// return; 
+
+// }
+
+// fun(30)
+
+// print 5 to 1
+
+
+
+
+//print even number 1 to 100
+// function even(n){
+//     if(n==0){
+//       return 0;
+//     }
+    
+//     even(n-1)
+//     if(n%2==0){
+//       console.log(n);
+//     }
+    
+    
+//     }
+//     even(100);
+
+
+
+
+//print odd number 1 to 100
+// function odd(n){
+//     if(n==0){
+//       return 0;
+//     }
+    
+//     odd(n-1)
+//     if(n%2!=0){
+//       console.log(n);
+//     }
+    
+    
+//     }
+//     odd(100);
+
+
+
+
+
+
+
+// factorial of 10
+// function fact(n){
+//     if(n==0){
+//     return 1;
+//     }
+     
+//      return n * fact(n-1);
+    
+    
+//     }
+//     var a=fact(10);
+//     console.log(a);
+
+    
